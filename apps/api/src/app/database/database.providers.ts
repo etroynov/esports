1c86903c9e3caaf53bd148e14be978f74c5d3b76
Evgeniy <etroynov@wynd.eu>
2019-07-24 18:13:37 +0300

    #master. fix docker

--- apps/api/src/app/database/database.providers.ts
+++ apps/api/src/app/database/database.providers.ts
@@ -1,9 +1,30 @@
+/**
+ * Vendor
+ */
+
 import * as mongoose from 'mongoose';
+
+/**
+ * Mongoose settings
+ */
+
+mongoose.set({ debug: true });
+
+/**
+ * Expo
+ */
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://root:example@localhost/esports', { useNewUrlParser: true }),
+      await mongoose.connect(
+        'mongodb://esports:pass@localhost/esports', {
+          useNewUrlParser: true,
+          auth: {
+            authdb:"admin"
+          }
+        }
+      ),
   },
 ];
--- apps/api/src/app/players/players.providers.ts
+++ apps/api/src/app/players/players.providers.ts
@@ -4,7 +4,9 @@ import { PlayerSchema } from './schemas/player.schema';
 export const playersProviders = [
   {
     provide: 'PLAYER_MODEL',
-    useFactory: (connection: Connection) => connection.model('Player', PlayerSchema),
+    useFactory: (connection: Connection) => connection.model(
+      'Player', PlayerSchema
+    ),
     inject: ['DATABASE_CONNECTION'],
   },
 ];

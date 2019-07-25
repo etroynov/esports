323f167281af34b6ef1526f71c1bc563fc592b38
Evgeniy Troynov <troinov174@gmail.com>
2019-07-26 02:41:01 +0300

    #master. change sass -> scss, change prefix lint

--- apps/ti/src/app/pages/teams/teams.component.ts
+++ apps/ti/src/app/pages/teams/teams.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.sass']
+  styleUrls: ['./teams.component.scss']
 })
 export class TeamsComponent implements OnInit {
 

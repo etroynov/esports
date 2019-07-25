22636a5d51e4a181c2c21ce2cb25f4279ab7f32b
Evgeniy Troynov <troinov174@gmail.com>
2019-07-25 08:01:09 +0300

    #master. add header, footer for ti app

--- apps/ti/src/app/app.module.ts
+++ apps/ti/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
